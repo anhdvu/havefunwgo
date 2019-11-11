da11d93562a5d78af911bd0f67829e30754d1580
anhdvu <mrandyvu@gmail.com>
2019-11-11 08:42:35 +0700

    fixed a minor bug which automatically added 0 at the beginning of result slice

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 my first attempt with golang
 
 I'm learning Go while my daughter is sleeping right next to me <3
+Go is not simple but very explicit for which I like it.
--- prime_numbers/prime_numbers.go
+++ prime_numbers/prime_numbers.go
@@ -7,25 +7,25 @@ import (
 
 // Beginning of main function to execute this script
 func main() {
-	fmt.Println(primeNumbers(53))
+	fmt.Println(primeNumbers(10))
 }
 
 func isPrime(number int) bool {
 	if number < 2 {
 		return false
 	}
-
-	for f := 2; f < int(math.Sqrt(float64(number)))+1; f++ {
-		if number%f == 0 {
-			return false
+	if number >= 2 {
+		for f := 2; f < int(math.Sqrt(float64(number)))+1; f++ {
+			if number%f == 0 {
+				return false
+			}
 		}
 	}
-
 	return true
 }
 
 func primeNumbers(n int) []int {
-	result := make([]int, 1)
+	result := make([]int, 0)
 
 	for i := 0; i <= n; i++ {
 		if isPrime(i) {
